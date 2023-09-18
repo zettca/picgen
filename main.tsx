bf731efb6e9d236e33de93c0386eef3a11941605
Bruno <zettca@users.noreply.github.com>
2023-09-18 22:58:03 +0100

    chore: add app route params

--- main.tsx
+++ main.tsx
@@ -1,12 +1,33 @@
+import { Hono } from "https://deno.land/x/hono@v3.6.3/mod.ts";
+import { logger } from "https://deno.land/x/hono@v3.6.3/middleware.ts";
+
 import { generate } from "./mod.tsx";
 
-Deno.serve(async (req) => {
-  const { searchParams } = new URL(req.url);
-  const params = Object.fromEntries(searchParams.entries());
+const safeNum = (str: string) => {
+  const num = Number(str);
+  return Number.isNaN(num) ? 32 : num;
+};
 
-  const headers: HeadersInit = {
-    "Content-Type": "image/svg+xml",
-  };
+const app = new Hono();
 
-  return new Response(await generate(params), { headers });
+app.use("*", logger());
+
+app.get("/", (ctx) => ctx.redirect("/420"));
+
+app.get("/:dims{[0-9]+(?:x[0-9]+)?}", async (ctx) => {
+  const dims = ctx.req.param("dims") || "200x200";
+  const [width, height] = dims.split("x").map(safeNum);
+  const params = { width, height, ...ctx.req.query() };
+
+  ctx.header("Content-Type", "image/svg+xml");
+  return ctx.body(await generate(params));
 });
+
+app.get("/*", async (ctx) => {
+  const params = { ...ctx.req.query() };
+
+  ctx.header("Content-Type", "image/svg+xml");
+  return ctx.body(await generate(params));
+});
+
+Deno.serve(app.fetch);
